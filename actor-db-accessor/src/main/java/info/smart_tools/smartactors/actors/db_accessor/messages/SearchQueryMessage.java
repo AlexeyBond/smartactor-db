5c3802f5b2e0f3cd54d8d7158172226929dccfd0
AlexeyBond <alexey.bond.94.55@gmail.com>
2015-08-18 17:00:27 +0600

    Add documentation for search and upsert messages interfaces.

--- actor-db-accessor/src/main/java/info/smart_tools/smartactors/actors/db_accessor/messages/SearchQueryMessage.java
+++ actor-db-accessor/src/main/java/info/smart_tools/smartactors/actors/db_accessor/messages/SearchQueryMessage.java
@@ -1,8 +1,35 @@
 package info.smart_tools.smartactors.actors.db_accessor.messages;
 
+/**
+ * Interface for message containing incoming search request.
+ *
+ * Query language ("query" field contents) rules:
+ * <pre>
+ *     query ::= queryGroup
+ *
+ *     // queryGroup which has no compositionType at left side is interpreted as a "$and" query group.
+ *     queryGroup ::= [queryGroup, ...]
+ *     queryGroup ::= {queryPair, ...}
+ *
+ *     // Works only outside a field context.
+ *     // Creates a field context with field fieldName for nested queryGroup.
+ *     queryPair ::= fieldName : queryGroup
+ *
+ *     // Works only inside of field context.
+ *     queryPair ::= constraintName : constraintParams
+ *
+ *     // Works everywhere.
+ *     queryPair ::= compositionType : queryGroup
+ *
+ *     constraintName ::= "$eq" | "$gt" | "$lt" | "$tagSet" | "$tagAll" | "$tagAny"
+ *
+ *     // "$not" actually works as NAND (because right part should always be a queryGroup.
+ *     compositionType ::= "$or" | "$and" | "$not"
+ * </pre>
+ */
 public interface SearchQueryMessage {
     String  getCollectionName();
     String  getTargetField();
 
-    /*TODO: Define search query format.*/
+    Object getQuery();
 }
--- actor-db-accessor/src/main/java/info/smart_tools/smartactors/actors/db_accessor/messages/UpsertQueryMessage.java
+++ actor-db-accessor/src/main/java/info/smart_tools/smartactors/actors/db_accessor/messages/UpsertQueryMessage.java
@@ -2,9 +2,19 @@ package info.smart_tools.smartactors.actors.db_accessor.messages;
 
 import info.smart_tools.smartactors.core.IObject;
 
+import java.util.List;
+
 public interface UpsertQueryMessage {
     String  getCollectionName();
 
-    /*TODO: How to use arrays/lists in messages? (Operations with multiple documents m.b. more efficient)*/
-    IObject getDocument();
+    /**
+     * Returns a list of objects to be upserted.
+     *
+     * @return a list of objects to be upserted.
+     *
+     * If a document in this list has "id" field and document with this identifier is exist in database the document
+     * in database will be updated, else the document will be inserted and id will be generated (if not provided in query).
+     * The generated id will be placed into document in response message.
+     */
+    List<IObject> getDocuments();
 }

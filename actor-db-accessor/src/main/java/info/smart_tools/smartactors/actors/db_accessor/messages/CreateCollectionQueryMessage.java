4813fbf302b8fd5cd8f54a494422a69c0c1611e1
AlexeyBond <alexey.bond.94.55@gmail.com>
2015-08-27 19:06:52 +0600

    Update collection creation query message interface.

--- actor-db-accessor/src/main/java/info/smart_tools/smartactors/actors/db_accessor/messages/CreateCollectionQueryMessage.java
+++ actor-db-accessor/src/main/java/info/smart_tools/smartactors/actors/db_accessor/messages/CreateCollectionQueryMessage.java
@@ -1,5 +1,9 @@
 package info.smart_tools.smartactors.actors.db_accessor.messages;
 
+import java.util.Map;
+
 public interface CreateCollectionQueryMessage {
     String  getCollectionName();
+
+    Map<String,String> getIndexes();
 }
